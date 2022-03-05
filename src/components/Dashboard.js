cba3f4f8e965e27abf201d9e18b77644808987d8
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-05 02:43:35 -0500

    update panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,21 +31,33 @@ class Dashboard extends Component {
     loading: false
   };
 
+  selectPanel(id) {
+    this.setState({
+      focused: id
+    });
+  }
+
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = data.filter(panel => this.state.focused === null || this.state.focused === panel.id
+    )
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={this.selectPanel}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
